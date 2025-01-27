8df07486206f6a7e37e9cc5fe32031334fb022e2
binyamin-zaidman <bl8843@gmail.com>
2025-01-27 21:58:48 +0200

    הכנסת תמויה יזכור לתוך הבלוק

--- ShemaIsrael/src/components/Navbar.jsx
+++ ShemaIsrael/src/components/Navbar.jsx
@@ -4,14 +4,9 @@ import "../styles/colors.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-color-gradient mb-1"> 
+    <nav className="navbar navbar-expand-lg bg-color-gradient mb-1">
       <div className="container-fluid">
-          <img
-            src="/images/logo.jpg"
-            alt="logo"
-            width="65"
-            className="rounded" 
-          />
+        <img src="/images/logo.jpg" alt="logo" width="65" className="rounded" />
         <button
           className="navbar-toggler"
           type="button"
@@ -26,22 +21,29 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active text-white m-3" href="https://www.guidestar.org.il/organization/580809119" target="_blank">
+              <a
+                className="nav-link active text-white m-3"
+                href="https://www.guidestar.org.il/organization/580809119"
+                target="_blank"
+              >
                 אודות
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link active text-white m-3" href="https://www.guidestar.org.il/organization/580809119" target="_blank">
+              <a
+                className="nav-link active text-white m-3"
+                href="https://www.guidestar.org.il/organization/580809119"
+                target="_blank"
+              >
                 ליצירת קשר
               </a>
             </li>
-            
           </ul>
+      <img src="/images/yizkor-22.jpg" className="yizkorImg rounded" alt="yizkor"/>
         </div>
       </div>
-      <img src="/images/yizkor-22.jpg" className="yizkorImg rounded" alt="yizkor" />
     </nav>
-  );
+  );  
 };
 
 export default Navbar;
