04485b3b5d15000fdd0b2d73bb244c7070751b42
binyamin-zaidman <bl8843@gmail.com>
2025-01-30 00:30:54 +0200

    redesign and add carousel for images

--- ShemaIsrael/src/components/ServiceCard.jsx
+++ ShemaIsrael/src/components/ServiceCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const ServiceCard = ({ title, description }) => (
   <div className="col-8 col-md-6 col-lg-6 p-3">
-    <div className="rounded p-3 h-100 bg-white shadow bg-color-gradient fade-in">
+    <div className="rounded p-3 h-100 bg-white shadow bg-color-gradient" >
       <h2 className="text-white">{title}</h2>
       {description && <p className="text-white">{description}</p>}
     </div>
