50bf21e14d90a036b2bc0b09456736c958ff28c8
binyamin-zaidman <bl8843@gmail.com>
2025-02-01 21:09:09 +0200

    תמונה חדשה

--- ShemaIsrael/src/components/Carousel.jsx
+++ ShemaIsrael/src/components/Carousel.jsx
@@ -6,7 +6,7 @@ const Carousel = () => {
   return (
     <div className="mt-2 d-flex flex-column justify-content-center align-items-center">
       <h2 className="ExplanatoryTitle rounded bg-color-gradient text-white p-2">
-        בואו לראות – סיורים שמאירים חותם{" "}
+        בואו לראות – סיורים שמשאירים חותם{" "}
       </h2>
       <div
         id="carouselExampleSlidesOnly"
@@ -16,21 +16,21 @@ const Carousel = () => {
         <div className="carousel-inner">
           <div className="carousel-item active">
             <img
-              src="./images/IMG_0197.JPG"
+              src="./images/IMG_0215.JPG"
               className="d-block w-100"
               alt="visitImage"
-            />
+              />
           </div>
           <div className="carousel-item">
             <img
               src="./images/IMG_1901.JPG"
               className="d-block w-100"
               alt="visitImage"
-            />
+              />
           </div>
           <div className="carousel-item">
             <img
-              src="./images/IMG_0215.JPG"
+              src="./images/IMG_0197.JPG"
               className="d-block w-100"
               alt="visitImage"
             />
